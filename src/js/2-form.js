2884a3cb02ed73f0b3d5fc7b6caa1a46e219cbef
MykolaSanych <ivanushkatt@gmail.com>
2024-04-30 02:51:21 +0300

    comit

---
+++ src/js/2-form.js
@@ -0,0 +1,28 @@
+const formData = { email: '', message: '' };
+const feedbackForm = document.querySelector('.feedback-form');
+const submitBtn = document.querySelector('.feedback-form>button');
+const feedbackFormState = JSON.parse(
+  localStorage.getItem('feedback-form-state')
+);
+if (feedbackFormState) {
+  feedbackForm.email.value = feedbackFormState.email;
+  feedbackForm.message.value = feedbackFormState.message;
+}
+feedbackForm.addEventListener('input', inputForm);
+function inputForm(evt) {
+  evt.preventDefault();
+  formData[evt.target.name] = evt.target.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}
+submitBtn.addEventListener('click', submit);
+function submit(evt) {
+  evt.preventDefault();
+  if (feedbackForm.email.value && feedbackForm.message.value) {
+    console.log(formData);
+    localStorage.removeItem('feedback-form-state');
+    feedbackForm.reset();
+    feedbackForm = {};
+  } else {
+    alert('«Fill please all fields»');
+  }
+}
